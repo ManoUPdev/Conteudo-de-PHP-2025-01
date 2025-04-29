4ebbff4fd84501fc52a9098d7cd007fd3268cd7e
ManoUpdev <emanuelcgalindo@hotmail.com>
2025-04-29 10:32:44 -0300

    comeco do php no alicerce

---
+++ AlicercePHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Formulário de Contato</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <div class="container">
+    <h1>Entre em Contato</h1>
+    
+    <?php
+
+// continuar
